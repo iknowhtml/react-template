73b43cfa4e2f443763beb6ceb05df485d32813ab
iknowhtml <iknowsmtp@gmail.com>
2020-01-25 11:18:51 -0500

    Refactor project structure

--- tests/components/App.test.js
+++ tests/components/App.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import App from './App';
+import App from '../../src/components/App';
 
 describe('App', () => {
   it('should render the App component', () => {
