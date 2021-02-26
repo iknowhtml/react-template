b2043a3f1982fe35df1e809982de68d2a44a9c81
iknowhtml <iknowsmtp@gmail.com>
2021-02-25 20:36:27 -0500

    Add React Hooks plugin to ESLint configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,11 +3,12 @@ module.exports = {
     ecmaVersion: 9,
     sourceType: 'module',
   },
-  plugins: ['prettier', 'react', 'jsx-a11y'],
+  plugins: ['prettier', 'react', 'jsx-a11y','react-hooks'],
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:jsx-a11y/recommended',
+    'plugin:react-hooks/recommended'
   ],
   rules: {
     'prettier/prettier': ['error'],
