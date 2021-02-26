03438858a9e345dd234b3633330ab80c54eb358a
iknowhtml <iknowsmtp@gmail.com>
2021-02-25 20:37:08 -0500

    Update code to reflect package upgrades

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,11 @@
 import React from 'react';
-import styled from '@emotion/styled';
+// import styled from '@emotion/styled';
 import GlobalStyles from './GlobalStyles';
-
-const Container = styled.div`
-  font-size: 2em;
-`;
 
 const App = () => (
   <>
     <GlobalStyles />
-    <Container>React Template</Container>
+    React Template
   </>
 );
 
--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -2,14 +2,11 @@ import path from 'path';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import { HotModuleReplacementPlugin } from 'webpack';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
-import HtmlWebpackTemplate from 'html-webpack-template';
 import TerserJSPlugin from 'terser-webpack-plugin';
 import OptimizeCssAssetsWebpackPlugin from 'optimize-css-assets-webpack-plugin';
 
-const isProduction = process.env.NODE_ENV === 'production';
-
 // Two parameters are passed in at bundle time, env & argv. argv contains all flags passed into webpack, including mode.
-const webpackConfiguration = () => ({
+const webpackConfiguration = (_, argv) => ({
   entry: path.resolve('src', 'index.js'),
   output: {
     path: path.resolve('dist'),
@@ -31,7 +28,7 @@ const webpackConfiguration = () => ({
           {
             loader: MiniCssExtractPlugin.loader,
             options: {
-              hmr: isProduction,
+              hmr: argv.mode === 'development',
             },
           },
           'css-loader',
@@ -44,22 +41,28 @@ const webpackConfiguration = () => ({
   plugins: [
     new HotModuleReplacementPlugin(),
     new HtmlWebpackPlugin({
-      title: '',
-      template: HtmlWebpackTemplate,
+      // inject: false,
+      templateContent: ({ htmlWebpackPlugin }) =>
+        `<html>
+          <head></head>
+          <body>
+            <div id="${htmlWebpackPlugin.options.appMountId}"></div>
+          </body>
+        </html>`,
       appMountId: 'app',
-      minify: isProduction
-        ? {
-            collapseWhitespace: true,
-            removeComments: true,
-            removeRedundantAttributes: true,
-            removeScriptTypeAttributes: true,
-            removeStyleLinkTypeAttributes: true,
-            useShortDoctype: true,
-          }
-        : false,
-      hash: isProduction,
+      minify:
+        argv.mode === 'production'
+          ? {
+              collapseWhitespace: true,
+              removeComments: true,
+              removeRedundantAttributes: true,
+              removeScriptTypeAttributes: true,
+              removeStyleLinkTypeAttributes: true,
+              useShortDoctype: true,
+            }
+          : false,
+      hash: argv.mode === 'production',
       //Prevents automatic injection of CSS & HTML into template.
-      inject: false,
     }),
     new MiniCssExtractPlugin({
       filename: 'style.css',
@@ -67,9 +70,10 @@ const webpackConfiguration = () => ({
   ],
   // Configures Optimizations
   optimization: {
-    minimizer: isProduction
-      ? [new TerserJSPlugin({}), new OptimizeCssAssetsWebpackPlugin({})]
-      : [],
+    minimizer:
+      argv.mode === 'production'
+        ? [new TerserJSPlugin({}), new OptimizeCssAssetsWebpackPlugin({})]
+        : [],
     splitChunks: {
       chunks: 'all',
     },
